2000f6c9023967533270fc3e039f63056d16e917
tomato23d <119089151+tomato23d@users.noreply.github.com>
2023-01-29 16:33:21 +1100

    Update script_02.js

--- assets/script_02.js
+++ assets/script_02.js
@@ -9,14 +9,14 @@ for (const top of topics) {console.log(top)};
 function listT() {for(var x = 0; x < topics.length; x++) {console.log(topics[x]);}};
 function selectT() {if (topics[0] === 'Dream') {console.log ("lets dream");} 
                     else if (topics[5] ==='Work') {console.log ("lets work");}
-                    else {console.log('foo');} };
+                    else {console.log('lets try again');} };
 
 var randomt = topics[Math.floor(Math.random()*topics.length)];
 function selectTR() {if     (randomt === 'Be Positive') {console.log ("no checks");}
                     else if (randomt === 'Exercise') {console.log ("eat well");}
                     else if (randomt === 'Dream') {console.log ("lets dream");} 
                     else if (randomt ==='Work') {console.log ("lets work");}
-                    else {console.log('foo');} };
+                    else {console.log('lets try again');} };
 
 
 
